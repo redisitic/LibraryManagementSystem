6136c9664c7aa10a46226fe8d1bc6d3d49918fda
Rhythm Juneja <66195447+redisitic@users.noreply.github.com>
2023-10-30 12:20:31 +0530

    Update EditBook.java

---
+++ src/EditBook.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class EditBook extends JFrame implements ActionListner{
+    Editbook(Book currentBook){
+        
+    }
+}
